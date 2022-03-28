a51b8cf3fc1baf3366ef234c3ac57759c771697e
Saori Mita <saorimita4@gmail.com>
2022-03-28 13:51:12 +0900

    fixed models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list, dependent: :destroy
-  validates :comment, length: { in: 0..6 }
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
